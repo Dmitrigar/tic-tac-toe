1be0e918ac0a7c8dcf1c4d418efaa23b948f10ad
Dmitriy Garbuz <dmigar@gmail.com>
2018-10-06 18:27:41 +0300

    Add ShoppingList component

---
+++ src/ShoppingList.js
@@ -0,0 +1,17 @@
+class ShoppingLIst extends React.Component {
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>Milk</li>
+          <li>Eggs</li>
+          <li>Bread</li>
+          <li>Watermelon</li>
+          <li>Oranges</li>
+          <li>Cabage</li>
+        </ul>
+      </div>
+    )
+  }
+}
