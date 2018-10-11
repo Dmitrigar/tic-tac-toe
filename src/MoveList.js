3571e9a918fbe6fdfee18ad7d7bc78c75663dd73
Dmitriy Garbuz <dmigar@gmail.com>
2018-10-11 17:08:10 +0300

    MoveList made extended class component.

--- src/MoveList.js
+++ src/MoveList.js
@@ -1,21 +1,35 @@
 import React from "react";
-function MoveList(props) {
-  return <ul className="move-list">{getMoves(props)}</ul>;
-}
 
+class MoveList extends React.Component {
+  render() {
+    return (
+      <div>
+        <button onClick={() => this.reverse()}>Reverse</button>
+        <ul className="move-list">{this.getMoves()}</ul>
+      </div>
+    );
+  }
 
-function getMoves(props) {
-  return props.history.map((step, move) => mapToMoveItem(props, step, move));
-}
+  reverse() {
+    alert("bam");
+    this.forceUpdate();
+  }
 
-function mapToMoveItem(props, step, move) {
-  const desc = props.getMoveDescription(step, move);
-  const className = move === props.selected ? "move--bold" : "move";
-  return (
-    <li key={move} className={className}>
-      <button onClick={() => props.clickMove(move)}>{desc}</button>
-    </li>
-  );
+  getMoves() {
+    return this.props.history.map((step, move) =>
+      this.mapToMoveItem(step, move)
+    );
+  }
+
+  mapToMoveItem(step, move) {
+    const desc = this.props.getMoveDescription(step, move);
+    const className = move === this.props.selected ? "move--bold" : "move";
+    return (
+      <li key={move} className={className}>
+        <button onClick={() => this.props.clickMove(move)}>{desc}</button>
+      </li>
+    );
+  }
 }
 
 export default MoveList;
